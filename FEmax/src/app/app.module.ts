f6378191d56bfde0e06a770ed4d67d9215f0d516
prafull <praguru14@gmail.com>
2022-03-27 17:16:57 +0530

    L5

--- FEmax/src/app/app.module.ts
+++ FEmax/src/app/app.module.ts
@@ -10,6 +10,7 @@ import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component
 import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
 
 
 
@@ -21,7 +22,8 @@ import { ShoppingListComponent } from './shopping-list/shopping-list.component';
     RecipeListComponent,
     RecipeDetailComponent,
     ShoppingEditComponent,
-    ShoppingListComponent
+    ShoppingListComponent,
+    RecipeItemComponent
   ],
   imports: [
     BrowserModule,
