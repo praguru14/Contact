a080e6e3ed0359aa4c93c47c4c69a57de7165242
prafull <praguru14@gmail.com>
2022-03-28 06:27:27 +0530

    L7

--- FEmax/src/app/shopping-list/shopping-list.component.ts
+++ FEmax/src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Ingredients } from '../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
@@ -6,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
+  ingredients:Ingredients[]=[
+    new Ingredients('Apples',5),  
+    new Ingredients('Tomatoes',10)
+  ];
 
   constructor() { }
 
